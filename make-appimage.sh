ff97e4e74b7b6a8b870c8f0fa93f25f59eb36dae
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-16 07:34:21 -0400

    deploy app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,23 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q rmg | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/com.github.Rosalie241.RMG.svg
+export DESKTOP=/usr/share/applications/com.github.Rosalie241.RMG.desktop
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+  /usr/bin/RMG       \
+  /usr/lib/RMG       \
+  /usr/lib/RMG/*     \
+  /usr/lib/RMG/*/*   \
+  /usr/lib/RMG/*/*/*
 
 # Additional changes can be done in between here
 
