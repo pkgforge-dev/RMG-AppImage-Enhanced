cef6a218de17e58341fa035a4403f369f8d07709
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-17 13:02:11 -0400

    use `RMG` as name of app

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,6 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q rmg | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
+export APPNAME=RMG
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/com.github.Rosalie241.RMG.svg
